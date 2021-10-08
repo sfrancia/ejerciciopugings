e80eb3320f109b7c44745b7b6446594c37776646
sfrancia <sebastianfrancia@hotmail.com>
2021-10-08 14:37:57 +0200

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
+
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },InAppBrowser,EmailComposer,Camera],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
